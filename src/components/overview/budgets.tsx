4a39b8fc8ccdf72f89924caad4430e6b06eb4282
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 13:58:41 +0800

    budgets page

---
+++ src/components/overview/budgets.tsx
@@ -0,0 +1,7 @@
+export function Budgets() {
+  return (
+    <div className="rounded-xl bg-white dark:bg-muted/50 shadow-sm p-4 mb-6">
+      <h4 className="text-lg font-bold">Budgets</h4>
+    </div>
+  );
+}

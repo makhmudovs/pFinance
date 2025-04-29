12d716e3837b96e3326c7d7cea5878c0426a74ef
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 13:56:22 +0800

    alertdialogprovider login/register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
+import { AlertDialogProvider } from "@/providers/AlertDialogProvider";
+import { Login } from "./pages/auth/login";
+import { Register } from "./pages/auth/register";
 import Overview from "./pages/Overview";
 import Transactions from "./pages/Transactions";
 import Budgets from "./pages/Budgets";
 import Pots from "./pages/Pots";
 import Bills from "./pages/Bills";
 
+
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="/" index element={<Overview />} />
-          <Route path="/transactions" index element={<Transactions />} />
-          <Route path="/budgets" index element={<Budgets />} />
-          <Route path="/pots" index element={<Pots />} />
-          <Route path="/bills" index element={<Bills />} />
-        </Route>
-      </Routes>
+      <AlertDialogProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" index element={<Overview />} />
+            <Route path="/transactions" index element={<Transactions />} />
+            <Route path="/budgets" index element={<Budgets />} />
+            <Route path="/pots" index element={<Pots />} />
+            <Route path="/bills" index element={<Bills />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AlertDialogProvider>
     </>
   );
 };

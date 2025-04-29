d550fc3851e99b862030bfd1847398a13660d1e8
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 14:00:29 +0800

    firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_APP_ID,
+  VITE_MEASUREMENT_ID,
+} = import.meta.env;
+
+//// TODO: Replace the following with your app's Firebase project configuration in env file
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_ID,
+  measurementId: VITE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };

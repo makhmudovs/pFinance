1ced2dd6908480a8cedeb0aa1370f5bca32fe102
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 13:59:28 +0800

    overview transactions

---
+++ src/components/overview/transactions.tsx
@@ -0,0 +1,7 @@
+export function Transactions() {
+  return (
+    <div className="rounded-xl bg-white dark:bg-muted/50 shadow-sm p-4">
+      <h4 className="text-lg font-bold">Transactions</h4>
+    </div>
+  );
+}

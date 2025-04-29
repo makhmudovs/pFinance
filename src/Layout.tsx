3c9d846a5de904333bca0f1e2614def951d0abb7
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 13:56:48 +0800

    user checked

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,57 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
+import { Navigate, Outlet } from "react-router-dom";
 import { AppSidebar } from "@/components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { ModeToggle } from "./components/mode-toggle";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Header } from "./components/header";
 
 const Layout = () => {
-  return (
+  const { user, loading } = useAuth();
+  if (loading) return <div>Loading...</div>;
+  
+  return user ? (
     <>
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex border-b justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">
-                      Building Your Application
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-            <div className="me-2">
-              <ModeToggle />
-            </div>
-          </header>
-          <div className="flex flex-1 flex-col gap-4 p-4">
-            <div className="aspect-video rounded-xl bg-muted/50 p-4">
+          <Header />
+          <div className="flex flex-1 flex-col gap-4">
+            {/* aspect-video rounded-xl bg-muted/50 */}
+            <div className="p-4">
               <Outlet />
             </div>
           </div>
         </SidebarInset>
       </SidebarProvider>
     </>
+  ) : (
+    <Navigate to="/login" replace />
   );
 };
 

fb92d1e29805ac872d933d0be6f1a232f57c85d1
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 13:58:31 +0800

    bills page

---
+++ src/components/overview/bills.tsx
@@ -0,0 +1,7 @@
+export function Bills() {
+  return (
+    <div className="rounded-xl bg-white dark:bg-muted/50 shadow-sm p-4">
+      <h4 className="text-lg font-bold">Recurring Bills</h4>
+    </div>
+  );
+}

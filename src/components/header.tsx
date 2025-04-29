87a3e3ef7711a7cab93ae8065a23837b29b57f86
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 13:57:38 +0800

    header

---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+import { Separator } from "@radix-ui/react-separator";
+import { ModeToggle } from "./mode-toggle";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "./ui/breadcrumb";
+import { SidebarTrigger } from "./ui/sidebar";
+
+export function Header() {
+  return (
+    <header className="flex border-b justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <div className="flex items-center gap-2 px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">
+                Building Your Application
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div className="me-2">
+        <ModeToggle />
+      </div>
+    </header>
+  );
+}

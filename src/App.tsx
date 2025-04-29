344d3e199734e4ecbb6a464eac601f80c3bd24ad
makhmudovs <makhmudovs50@gmail.com>
2025-04-29 23:38:28 +0800

    404 page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import { AlertDialogProvider } from "@/providers/AlertDialogProvider";
-import { Login } from "./pages/auth/login";
+import { Login } from "@/pages/auth/Login";
 import { Register } from "./pages/auth/register";
 import Overview from "./pages/Overview";
 import Transactions from "./pages/Transactions";
 import Budgets from "./pages/Budgets";
 import Pots from "./pages/Pots";
 import Bills from "./pages/Bills";
-
+import { NotFound } from "./pages/404/not-found";
 
 const App = () => {
   return (
@@ -17,13 +17,14 @@ const App = () => {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" index element={<Overview />} />
-            <Route path="/transactions" index element={<Transactions />} />
-            <Route path="/budgets" index element={<Budgets />} />
-            <Route path="/pots" index element={<Pots />} />
-            <Route path="/bills" index element={<Bills />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/budgets" element={<Budgets />} />
+            <Route path="/pots" element={<Pots />} />
+            <Route path="/bills" element={<Bills />} />
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </AlertDialogProvider>
     </>

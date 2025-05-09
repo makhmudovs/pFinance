c01d7239c86fdd3d48f1151c8aa9aaf73b1df287
makhmudovs <makhmudovs50@gmail.com>
2025-05-09 13:43:58 +0800

    feat:location and transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
+import { Route, Routes, useLocation } from "react-router-dom";
+
+import { Layout } from "./Layout";
 import { AlertDialogProvider } from "@/providers/AlertDialogProvider";
 import { Login } from "@/pages/auth/Login";
-import { Register } from "./pages/auth/register";
-import Overview from "./pages/Overview";
-import Transactions from "./pages/Transactions";
-import Budgets from "./pages/Budgets";
-import Pots from "./pages/Pots";
-import Bills from "./pages/Bills";
+import { Register } from "./pages/auth/Register";
+import { Overview } from "./pages/Overview";
+import { Transactions } from "./pages/Transactions";
+import { Budgets } from "./pages/Budgets";
+import { Pots } from "./pages/Pots";
+import { Bills } from "./pages/Bills";
 import { NotFound } from "./pages/404/not-found";
+import { TransactionModal } from "./components/transactions/transaction-modal";
 
 const App = () => {
+  const location = useLocation();
+  const previousLocation = location.state?.previousLocation;
   return (
     <>
       <AlertDialogProvider>
-        <Routes>
+        <Routes location={previousLocation || location}>
           <Route element={<Layout />}>
             <Route path="/" index element={<Overview />} />
             <Route path="/transactions" element={<Transactions />} />
@@ -26,6 +30,12 @@ const App = () => {
           <Route path="/register" element={<Register />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        {/* Show the modal when a background location is set */}
+        {previousLocation && (
+          <Routes>
+            <Route path="/transactions/:id" element={<TransactionModal />} />
+          </Routes>
+        )}
       </AlertDialogProvider>
     </>
   );
